ee6f51dc4f9ff08502dafda80374591d0f886274
amruthasimhadri <132999334+amruthasimhadri@users.noreply.github.com>
2024-02-07 12:55:14 +0530

    .

--- SplitWiseApp/Controllers/SplitWiseController.cs
+++ SplitWiseApp/Controllers/SplitWiseController.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SplitWiseApp.Models;
 
 namespace SplitWiseApp.Controllers
 {
     public class SplitWiseController : Controller
     {
+        private readonly ApiService _apiService;
+        public SplitWiseController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserModel user)
+        {
+            _apiService.AddUser(user);
+            ViewBag.SuccessMessage = "Registered Successfully";
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
