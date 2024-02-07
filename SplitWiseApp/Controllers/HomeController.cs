270b588520ca6645e90a84f25568c5e417b66448
amruthasimhadri <132999334+amruthasimhadri@users.noreply.github.com>
2024-02-07 12:35:46 +0530

    .

--- SplitWiseApp/Controllers/HomeController.cs
+++ SplitWiseApp/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace SplitWiseApp.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
