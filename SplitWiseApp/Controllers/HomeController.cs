4a31fd04ed3a9945e8a8ca1448a64d2630f9d1da
SwathikaMalla <137275622+SwathikaMalla@users.noreply.github.com>
2024-02-07 12:35:51 +0530

    m

--- SplitWiseApp/Controllers/HomeController.cs
+++ SplitWiseApp/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace SplitWiseApp.Controllers
         {
             return View();
         }
-
+        public IActionResult Login()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
